e2fb0fad26e736150c8e3c0d60f06934cef2fb74
jyoti-varshney7 <jyoti.varshney_cs18@gla.ac.in>
2020-05-22 23:51:23 +0530

    Initial commit

---
+++ app/src/main/java/e/meghna/musicplayercontroller/MainActivity.java
@@ -0,0 +1,33 @@
+package e.meghna.musicplayercontroller;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+    MediaPlayer pianoMusic;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pianoMusic = MediaPlayer.create(this,R.raw.piano);
+        Switch switch_looping = (Switch) findViewById(R.id.switch_looping);
+        switch_looping.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                pianoMusic.setLooping(isChecked);
+            }
+        });
+    }
+    public void playMusic(View v){
+        pianoMusic.start();
+    }
+    public void pauseMusic(View v){
+        if(pianoMusic.isPlaying())
+             pianoMusic.pause();
+
+    }
+}
